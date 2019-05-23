77637da423acb43c19aa0b998ea32bece3938d4d
Jasmine Chen <jasmine1226@gmail.com>
2019-05-23 04:44:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
 # your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  array.select{|item| item[0]=='r'} == array ? true : false
+end
+
+def contain_a(array)
+  array.select{|item| item.include?'a'}
+end
+
+def first_wa(array)
+  array.find{|item| item.match(/wa\w*/)}
+end
+
+def remove_non_strings(array)
+  array.keep_if{|item| item.is_a?(String)}
+end
+
+def count_elements(array)
+  counts = array.each_with_object(Hash.new(0)) { |h1, h2| h2[h1] += 1 }
+  output = []
+  counts.each do |key, value|
+    key[:count] = value
+    output << key
+  end
+  output
+end
+
+def merge_data(keys, data)
+  keys.collect do |key, value|
+    puts "key = #{key}"
+    puts "value = #{value}"
+    data.each do |data_k, data_v|
+      puts "data_k = #{data_k}"
+      puts "data_v = #{data_v}"
+      if data_k == value
+        keys.merge(data_v)
+
+        puts keys
+      end
+    end
+  end
+  keys
+end
