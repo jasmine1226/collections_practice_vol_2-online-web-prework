0f96176e07df07d713dd483f157f64d385a15467
Jasmine Chen <jasmine1226@gmail.com>
2019-05-23 04:56:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,7 +28,7 @@ def count_elements(array)
 end
 
 def merge_data(keys, data)
-  names = keys.values_at(:first_name)
+  names = keys.values_at(:first_name).to_i
   puts names
   i = 0
   puts data[0]
